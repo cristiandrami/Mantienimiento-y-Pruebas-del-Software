ba31701c7809015edf936a7e910389b986d4fd18
cristiandrami <cristiandramisino@libero.it>
2022-02-25 11:59:29 +0100

    new branch

--- Factorial/src/main/java/org/mps2022/factorial/Factorial.java
+++ Factorial/src/main/java/org/mps2022/factorial/Factorial.java
@@ -8,7 +8,6 @@ public class Factorial {
 
     public int compute(int numberToCompute) {
         //i use 1 so i can do the factorial of 0 without check it
-
         int factorial=1;
         if(numberToCompute < 0){
             throw new RuntimeException("The value is negative: " + numberToCompute);

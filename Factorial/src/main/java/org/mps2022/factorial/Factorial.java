a6ca662a7a5744f20f617147d5c0c321602592d2
cristiandrami <cristiandramisino@libero.it>
2022-02-25 11:49:52 +0100

    indentation adjusted

--- Factorial/src/main/java/org/mps2022/factorial/Factorial.java
+++ Factorial/src/main/java/org/mps2022/factorial/Factorial.java
@@ -4,16 +4,14 @@ public class Factorial {
 
     public int compute(int numberToCompute) {
         //i use 1 so i can do the factorial of 0 without check it
+
         int factorial=1;
         if(numberToCompute < 0){
             throw new RuntimeException("The value is negative: " + numberToCompute);
         }
-
         for(int i=1; i<=numberToCompute; i++){
             factorial*=i;
         }
-
-
         return factorial;
     }
 }
